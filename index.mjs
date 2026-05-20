e4e6d39dc6a5267d113de5da682bf47e0a25e58f
Medhakesavan <medhakesavan@gmail.com>
2026-05-20 13:06:10 +0530

    first commit

--- index.mjs
+++ index.mjs
@@ -92,6 +92,17 @@ const corsOptions = {
 app.use(helmet());
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,Accept,Origin');
+  }
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
